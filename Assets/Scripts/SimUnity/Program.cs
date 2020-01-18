2679f49e720d55815f1e2e6bea005d9e3f43ec63
LazyDuchess <Nahuel3d@gmail.com>
2020-01-18 00:03:26 -0300

    some comments

--- Assets/Scripts/SimUnity/Program.cs
+++ Assets/Scripts/SimUnity/Program.cs
@@ -13,26 +13,32 @@ public class Program : MonoBehaviour
     }
     IEnumerator Init()
     {
+        //Initialize Initialization
         Environment.InitInit();
+        //Load startup files
         Environment.LoadArchives(Environment.config.archives_startup);
+        //music
         var ob = new GameObject("Background Music");
         GameObject.DontDestroyOnLoad(ob);
         Environment.bgMusic = ob.AddComponent<Music>();
         Environment.bgMusic.Play();
+        //Load mods and main archives
         StartCoroutine(Environment.Init());
-        
+        //Startup loading screen
         var startupLoadPanel = Panel.StartupLoading();
-        while (!Environment.LoadFinished)
+        
+        while (!Environment.LoadFinished) // Only do the following things after main archives are loaded
             yield return null;
-        var hoods_folder = Path.Combine(Environment.config.user_dir, "Neighborhoods");
-        DirectoryInfo hoodInfo = new DirectoryInfo(hoods_folder);
+
+        var hoods_folder = Path.Combine(Environment.config.user_dir, "Neighborhoods"); //Neighborhoods folder
+        DirectoryInfo hoodInfo = new DirectoryInfo(hoods_folder); 
         foreach (var dire in hoodInfo.GetDirectories())
         {
             var hd = new Neighborhood(dire.FullName);
-            Environment.hoods.Add(hd);
+            Environment.hoods.Add(hd);   //Load hoods
         }
-        startupLoadPanel.Delete();
+        startupLoadPanel.Delete(); //Remove loading panel since it's done
         var menu = new GameObject();
-        menu.AddComponent<MainMenu>();
+        menu.AddComponent<MainMenu>(); //Start main menu!
     }
 }
